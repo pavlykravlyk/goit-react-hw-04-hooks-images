ed304c87d7e58cb5fa2012f1714806460e28affa
Pavlo Kravchuk <kravchuk.pavlo@ukr.net>
2022-01-30 21:53:50 +0100

    Update pixabay-api.jsx

--- src/services/pixabay-api.jsx
+++ src/services/pixabay-api.jsx
@@ -1,22 +1,30 @@
 import PropTypes from 'prop-types';
+import axios from 'axios';
 
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '23902495-d255dd7217da8bb07f7abae59';
 
-export default function fetchImages(searchQuery, page) {
+const fetchImages = async (query, page) => {
   const searchParams = new URLSearchParams({
     key: API_KEY,
     orientation: 'horizontal',
     image_type: 'photo',
-    q: searchQuery,
+    q: query,
     page: page,
     per_page: 12,
   });
 
-  return fetch(`${BASE_URL}?${searchParams}`);
-}
+  try {
+    const { data } = await axios.get(`${BASE_URL}?${searchParams}`);
+    return data.hits;
+  } catch (error) {
+    return error;
+  }
+};
 
 fetchImages.propTypes = {
-  searchQuery: PropTypes.string,
+  query: PropTypes.string,
   page: PropTypes.number,
 };
+
+export default fetchImages;
