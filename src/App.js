27ce7f6de86d066fcd86932fcd874ba4fc0fa35f
Pavlo Kravchuk <kravchuk.pavlo@ukr.net>
2022-01-30 21:53:46 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ import Modal from './components/Modal';
 import Searchbar from './components/Searchbar';
 import ImageGallery from './components/ImageGallery';
 import Button from './components/Button';
-import PixabayApiService from './services/pixabay-api';
+import API from './services/pixabay-api';
 
 export default function App() {
-  const [searchQuery, setSearchQuery] = useState('');
+  const [query, setQuery] = useState('');
   const [showModal, setShowModal] = useState(false);
   const [page, setPage] = useState(1);
   const [images, setImages] = useState([]);
@@ -20,39 +20,44 @@ export default function App() {
   const [status, setStatus] = useState('idle');
 
   useEffect(() => {
-    if (searchQuery) {
+    const findImageByName = async () => {
+      try {
+        const response = await API(query);
+        setImages(response);
+        setStatus('resolved');
+      } catch (error) {
+        setError(error);
+        setStatus('rejected');
+        toast.error(error);
+      }
+    };
+
+    if (query) {
       setStatus('pending');
       setPage(1);
       setImages([]);
       findImageByName();
-      scroll.scrollToBottom();
     }
-  }, [searchQuery]);
+  }, [query]);
 
   useEffect(() => {
-    if (searchQuery && page !== 1) {
+    const findImageByName = async () => {
+      try {
+        const response = await API(page);
+        setImages(state => [...state, ...response]);
+        setStatus('resolved');
+      } catch (error) {
+        setError(error);
+        setStatus('rejected');
+        toast.error(error);
+      }
+    };
+
+    if (page > 1) {
       setStatus('pending');
       findImageByName();
-      scroll.scrollToBottom();
     }
   }, [page]);
-
-  const findImageByName = async () => {
-    try {
-      const response = await PixabayApiService(searchQuery, page);
-      if (response.ok) {
-        const articles = await response.json();
-        setImages(prevState => [...prevState, ...articles.hits]);
-        setStatus('resolved');
-      } else {
-        return Promise.reject(new Error(`No matches found for ${searchQuery}`));
-      }
-    } catch (error) {
-      setError(error);
-      setStatus('rejected');
-      toast.error('Input field must not be empty');
-    }
-  };
 
   const toggleModal = image => {
     setShowModal(!showModal);
@@ -61,28 +66,25 @@ export default function App() {
 
   return (
     <div className={styles.App}>
-      <Searchbar onSubmit={setSearchQuery} />
+      <Searchbar onSubmit={setQuery} />
       {status === 'idle' && <div>Free images</div>}
-
       {status === 'rejected' && <h1>{error.message}</h1>}
-
+      {status === 'resolved' && scroll.scrollToBottom()}
       {status === 'resolved' && (
         <>
           <ImageGallery images={images} onOpenModal={toggleModal} />
           {images.length !== 0 && (
             <Button onLoadMore={() => setPage(prevState => prevState + 1)} />
           )}
-          {images.length === 0 && <div>{searchQuery} not found</div>}
+          {images.length === 0 && <div>{query} not found</div>}
         </>
       )}
-
       {status === 'pending' && (
         <>
           <ImageGallery images={images} onOpenModal={toggleModal} />
           <Loader type="ThreeDots" color="#3f51b5" height={80} width={80} />
         </>
       )}
-
       {showModal && (
         <Modal onClose={toggleModal}>
           <img src={currImg.largeImageURL} alt={currImg.tags} />
